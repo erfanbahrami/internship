c0a80becd2958be830c0a5adbb1c6b4315decc6d
erfan bahrami <erfanbahrami1999@gmail.com>
2021-09-08 20:02:20 +0430

    update code

--- display pop-up menu and send emails/code.py
+++ display pop-up menu and send emails/code.py
@@ -1,40 +1,46 @@
 import tkinter
 import socket
 import sys
+import time
 import smtplib
- 
+
+class A:
+   #creates parent window
+   def __init__(self):    
+      self.root = tkinter.Tk()
+      self.root.geometry('500x500')
+      self.frame1 = tkinter.Label(self.root,width = 400,height = 400,bg = '#AAAAAA')
+      self.frame1.pack()
+
+   def hey(self,s):
+      self.frame1.configure(text = s)
+
+   def run(self,data):
+      self.hey(data)
+      self.root.after(5000, lambda: self.root.destroy()) # Destroy the widget after 30 seconds
+      self.root.mainloop()
 
 if __name__ == '__main__':
-   HOST = '127.0.0.1' 
-   PORT = 5789
-
-   while True:
-      a = tkinter.Tk()
-      a.geometry('350x350')
-      a.frame1 = tkinter.Label(a,width = 400,height = 400,bg = '#AAAAAA')
-      a.frame1.pack() 
+    while True:
+      a = A()
+      HOST = '127.0.0.1' 
+      PORT = 5789
       soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
    
       try:
-         soc.bind((HOST, PORT))
-       
+        soc.bind((HOST, PORT))
       except socket.error as massage:
-         print('Bind failed. Error Code : ' + str(massage[0]) + ' Message ' + massage[1])
-         sys.exit()
-          
-      print('Socket binding operation completed')
+        print('Bind failed. Error Code : ' + str(massage[0]) + ' Message ' + massage[1])
+        sys.exit()
    
       soc.listen(9)
    
       conn, address = soc.accept()
       print('Connected with ' + address[0] + ':' + str(address[1]))
 
-   
       data = conn.recv(1024).decode()
       soc.close()
-      a.frame1.configure(text = data)
-      a.after(10000, lambda: a.destroy())
-      a.mainloop()
+      a.run(data)
 
       ##send email
       gmail_user = "erfanbahrami1999@gmail.com"
@@ -64,12 +70,7 @@ if __name__ == '__main__':
          print("Something went wrong....",ex) 
 
       if not data:
-         break
+        break
 
   
-
    
-
-
-  
-  
